502db9662e82b1ecf06a9c9162224f6376c2aa83
brill3000 <bkaboyee@gmail.com>
2023-12-18 19:18:37 +0300

    code clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,6 @@ export const metadata = {
   description:
     'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
   keywords: 'react,material,kit,application,dashboard,admin,template',
-  themeColor: '#000000',
   manifest: '/manifest.json',
   viewport: { width: 'device-width', initialScale: 1, maximumScale: 1 },
   icons: [
