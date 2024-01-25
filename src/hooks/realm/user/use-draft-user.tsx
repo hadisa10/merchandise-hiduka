b9782889f1cf7396929a953817e5e4e122539624
brill3000 <brilliant@hokela.co.ke>
2024-01-25 07:47:58 +0300

    user mutations and query

---
+++ src/hooks/realm/user/use-draft-user.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { IDraftUser, IDraftUsersHook } from "src/types/user_realm";
+
+import { createObjectId } from "src/utils/realm";
+
+export function useDraftUsers(): IDraftUsersHook {
+  const [drafts, setDrafts] = useState<IDraftUser[]>([]);
+
+  const createDraftUser = () => {
+    const draftTodo: IDraftUser = {
+      _id: createObjectId(),
+      email: "",
+      isPublic: false,
+      displayName: "",
+      city: "",
+      state: "",
+      about: "",
+      country: "",
+      address: "",
+      zipCode: "",
+      role: "",
+      phoneNumber: "",
+      photoURL: "",
+      active: false
+    };
+    setDrafts((d) => [...d, draftTodo]);
+  };
+
+  const setDraftUserName = (draft: IDraftUser, displayName: string) => {
+    setDrafts((oldDrafts) => {
+      const idx = oldDrafts.findIndex((d) => d._id === draft._id);
+      return [
+        ...oldDrafts.slice(0, idx),
+        { ...oldDrafts[idx], displayName },
+        ...oldDrafts.slice(idx + 1),
+      ];
+    });
+  };
+
+  const deleteDraftUser = (draft: IDraftUser) => {
+    setDrafts((oldDrafts) => {
+      const idx = oldDrafts.findIndex((d) => d._id === draft._id);
+      return [...oldDrafts.slice(0, idx), ...oldDrafts.slice(idx + 1)];
+    });
+  };
+
+  return {
+    draftUsers: drafts,
+    createDraftUser,
+    setDraftUserName,
+    deleteDraftUser,
+  };
+}
