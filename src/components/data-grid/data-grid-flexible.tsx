bddad72233800ab057ceca2fe042702df63afab4
brill3000 <brilliant@hokela.co.ke>
2024-02-23 17:45:49 +0300

    fix some answer issues

--- src/components/data-grid/data-grid-flexible.tsx
+++ src/components/data-grid/data-grid-flexible.tsx
@@ -1,7 +1,150 @@
+// import React, { useMemo, useState } from 'react';
+
+// import { Box } from '@mui/system';
+// import { Typography } from '@mui/material';
+// import {
+//   DataGrid,
+//   GridColDef,
+//   GridRowModel,
+//   GridRowIdGetter,
+//   GridToolbarExport,
+//   GridToolbarContainer,
+//   GridRowSelectionModel,
+//   GridToolbarQuickFilter,
+//   GridToolbarFilterButton,
+//   GridToolbarColumnsButton,
+//   GridColumnVisibilityModel,
+//   GridToolbarDensitySelector,
+// } from '@mui/x-data-grid';
+
+// import { fDate } from 'src/utils/format-time';
+
+// import EmptyContent from 'src/components/empty-content/empty-content';
+
+// // Additional imports remain the same
+
+// // Define generic types for rows and columns
+// interface DataGridFlexibleProps<RowType extends GridRowModel> {
+//   data: RowType[];
+//   getRowIdFn: GridRowIdGetter<RowType>
+// }
+
+// // ----------------------------------------------------------------------
+
+
+// function generateDynamicColumns<RowType extends GridRowModel>(data: RowType[]): GridColDef[] {
+//   if (data.length === 0) {
+//     return [];
+//   }
+
+//   const sampleRow = data[0];
+//   return Object.keys(sampleRow).map((key) => ({
+//     field: key,
+//     headerName: key.charAt(0).toUpperCase() + key.slice(1),
+//     flex: 1,
+//     minWidth: 200,
+//     renderCell: (params) => {
+//       const { value } = params;
+//       switch (typeof value) {
+//         case 'boolean':
+//           return <Typography variant='body2'>{value ? 'Yes' : 'No'}</Typography>;
+//         case 'number':
+//           return <Typography variant='body2'>{value}</Typography>;
+//         case 'object':
+//           if (value instanceof Date) {
+//             return <Typography variant='body2'>{fDate(value)}</Typography>;
+//           }
+//           return <Typography variant='body2'>{JSON.stringify(value)}</Typography>;
+//         default:
+//           return <Typography variant='body2'>{value}</Typography>;
+//       }
+//     }
+//   }));
+// }
+
+// const HIDE_COLUMNS = {
+//   _id: false,
+// };
+
+// const HIDE_COLUMNS_TOGGLABLE = ['_id', 'actions'];
+
+// // ----------------------------------------------------------------------
+
+
+// // Use these generic types in your component
+// export default function DataGridFlexible<RowType extends GridRowModel>({
+//   data: rows,
+//   getRowIdFn
+// }: DataGridFlexibleProps<RowType>) {
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   const [selectedRows, setSelectedRows] = useState<GridRowSelectionModel>([]);
+
+//   const [columnVisibilityModel, setColumnVisibilityModel] =
+//     useState<GridColumnVisibilityModel>(HIDE_COLUMNS);
+
+//   const columns = useMemo(() => generateDynamicColumns(rows), [rows]);
+
+//   const getTogglableColumns = () =>
+//     columns
+//       .filter((column) => !HIDE_COLUMNS_TOGGLABLE.includes(column.field))
+//       .map((column) => column.field);
+
+//   // const selected = rows.filter((row) => selectedRows.includes(row.id)).map((_row) => _row.id);
+
+//   return (
+//     <DataGrid
+//       getRowId={getRowIdFn}
+//       checkboxSelection
+//       disableRowSelectionOnClick
+//       rows={rows}
+//       columns={columns}
+//       onRowSelectionModelChange={(newSelectionModel) => {
+//         setSelectedRows(newSelectionModel);
+//       }}
+//       columnVisibilityModel={columnVisibilityModel}
+//       onColumnVisibilityModelChange={(newModel) => setColumnVisibilityModel(newModel)}
+//       slots={{
+//         toolbar: CustomToolbar,
+//         noRowsOverlay: () => <EmptyContent title="No Data" />,
+//         noResultsOverlay: () => <EmptyContent title="No results found" />,
+//       }}
+//       slotProps={{
+//         toolbar: {
+//           showQuickFilter: true,
+//         },
+//         columnsPanel: {
+//           getTogglableColumns,
+//         },
+//       }}
+//     />
+
+//   )
+
+//   // Component implementation remains largely the same
+// }
+
+// // ----------------------------------------------------------------------
+
+
+// function CustomToolbar() {
+//   return (
+//     <GridToolbarContainer>
+//       <GridToolbarQuickFilter />
+//       <Box sx={{ flexGrow: 1 }} />
+//       <GridToolbarColumnsButton />
+//       <GridToolbarFilterButton />
+//       <GridToolbarDensitySelector />
+//       <GridToolbarExport />
+//     </GridToolbarContainer>
+//   );
+// }
+
+
+
+import { isObject, isString } from 'lodash';
 import React, { useMemo, useState } from 'react';
 
-import { Box } from '@mui/system';
-import { Typography } from '@mui/material';
+import { Box, Stack, Avatar, Typography } from '@mui/material';
 import {
   DataGrid,
   GridColDef,
@@ -21,75 +164,121 @@ import { fDate } from 'src/utils/format-time';
 
 import EmptyContent from 'src/components/empty-content/empty-content';
 
-// Additional imports remain the same
+import Iconify from '../iconify';
+import Label, { LabelColor } from '../label';
 
-// Define generic types for rows and columns
+
+interface IColumnValueOption {
+  value: any; // The value to match
+  label: string; // The label to display
+  color?: LabelColor; // Optional color for the label
+}
+// Updated interface for column definitions
+export interface IColumn {
+  field: string;
+  label: string;
+  order: number;
+  type: string;
+  minWidth?: number;
+  valueOptions?: IColumnValueOption[]
+}
+
+type IColumnsArray = IColumn[];
+
+// Updated props interface to include the columns array
 interface DataGridFlexibleProps<RowType extends GridRowModel> {
   data: RowType[];
-  getRowIdFn: GridRowIdGetter<RowType>
+  getRowIdFn: GridRowIdGetter<RowType>;
+  columns: IColumnsArray;
+  hideColumn?: Record<string, boolean>;
 }
 
-// ----------------------------------------------------------------------
-
-
-function generateDynamicColumns<RowType extends GridRowModel>(data: RowType[]): GridColDef[] {
-  if (data.length === 0) {
-    return [];
-  }
-
-  const sampleRow = data[0];
-  return Object.keys(sampleRow).map((key) => ({
-    field: key,
-    headerName: key.charAt(0).toUpperCase() + key.slice(1),
-    flex: 1,
-    minWidth: 200,
-    renderCell: (params) => {
-      const { value } = params;
-      switch (typeof value) {
-        case 'boolean':
-          return <Typography variant='body2'>{value ? 'Yes' : 'No'}</Typography>;
-        case 'number':
-          return <Typography variant='body2'>{value}</Typography>;
-        case 'object':
-          if (value instanceof Date) {
-            return <Typography variant='body2'>{fDate(value)}</Typography>;
-          }
-          return <Typography variant='body2'>{JSON.stringify(value)}</Typography>;
-        default:
-          return <Typography variant='body2'>{value}</Typography>;
-      }
-    }
-  }));
+// Function to generate dynamic columns based on the columns array
+function generateDynamicColumns(columnsArray: IColumnsArray): GridColDef[] {
+  return columnsArray
+    .sort((a, b) => a.order - b.order)
+    .map((column) => ({
+      field: column.field,
+      headerName: column.label,
+      valueOptions: Array.isArray(column.valueOptions) ? column.valueOptions.map(c => c.value) : [],
+      flex: 1,
+      minWidth: column.minWidth ?? 200,
+      renderCell: (params) => {
+        const { value } = params;
+        switch (column.type) {
+          case 'main':
+            return (
+              <Stack spacing={2} direction="row" alignItems="center" sx={{ minWidth: 0 }}>
+                <Avatar alt={value} sx={{ width: 36, height: 36 }} variant='rounded'>
+                  {isString(value) && value.charAt(0).toUpperCase()}
+                </Avatar>
+                <Typography component="span" variant="body2" noWrap>
+                  {value}
+                </Typography>
+              </Stack>
+            )
+          case 'select':
+            if (Array.isArray(column.valueOptions)) {
+              const matchedOption = column.valueOptions.find(option => option.value === value);
+              if (matchedOption) {
+                // If there's a matched option, you can use its label and color for rendering
+                return (
+                  <Label
+                    variant="soft"
+                    color={matchedOption.color}
+                    sx={{ mx: 'auto' }}
+                  >
+                    {value}
+                  </Label>
+                );
+              }
+            }
+            return (
+              <Label
+                variant="soft"
+                color="default"
+                sx={{ mx: 'auto' }}
+              >
+                {value}
+              </Label>
+            )
+          case 'boolean':
+            return (
+              value ? (
+                <Iconify icon="eva:checkmark-circle-2-fill" sx={{ color: 'primary.main' }} />
+              ) : (
+                '-'
+              )
+            )
+          case 'number':
+            return <Typography variant='body2'>{value}</Typography>;
+          case 'array':
+            return <Typography variant='body2'>{Array.isArray(value) && value.join(",")}</Typography>;
+          case 'date':
+            return <Typography variant='body2'>{fDate(new Date(value))}</Typography>;
+          case 'object':
+            return <Typography variant='body2'>{JSON.stringify(value)}</Typography>;
+          default:
+            return <Typography variant='body2'>{value}</Typography>;
+        }
+      },
+    }));
 }
 
-const HIDE_COLUMNS = {
-  _id: false,
-};
-
-const HIDE_COLUMNS_TOGGLABLE = ['_id', 'actions'];
-
-// ----------------------------------------------------------------------
-
-
-// Use these generic types in your component
+// The DataGridFlexible component
 export default function DataGridFlexible<RowType extends GridRowModel>({
   data: rows,
-  getRowIdFn
+  getRowIdFn,
+  columns: columnsArray,
+  hideColumn
 }: DataGridFlexibleProps<RowType>) {
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [selectedRows, setSelectedRows] = useState<GridRowSelectionModel>([]);
 
-  const [columnVisibilityModel, setColumnVisibilityModel] =
-    useState<GridColumnVisibilityModel>(HIDE_COLUMNS);
+  const [columnVisibilityModel, setColumnVisibilityModel] = useState<GridColumnVisibilityModel>(isObject(hideColumn) ? hideColumn : {});
 
-  const columns = useMemo(() => generateDynamicColumns(rows), [rows]);
-
-  const getTogglableColumns = () =>
-    columns
-      .filter((column) => !HIDE_COLUMNS_TOGGLABLE.includes(column.field))
-      .map((column) => column.field);
-
-  // const selected = rows.filter((row) => selectedRows.includes(row.id)).map((_row) => _row.id);
+  const columns = useMemo(() => generateDynamicColumns(columnsArray), [columnsArray]);
 
   return (
     <DataGrid
@@ -98,11 +287,9 @@ export default function DataGridFlexible<RowType extends GridRowModel>({
       disableRowSelectionOnClick
       rows={rows}
       columns={columns}
-      onRowSelectionModelChange={(newSelectionModel) => {
-        setSelectedRows(newSelectionModel);
-      }}
+      onRowSelectionModelChange={setSelectedRows}
       columnVisibilityModel={columnVisibilityModel}
-      onColumnVisibilityModelChange={(newModel) => setColumnVisibilityModel(newModel)}
+      onColumnVisibilityModelChange={setColumnVisibilityModel}
       slots={{
         toolbar: CustomToolbar,
         noRowsOverlay: () => <EmptyContent title="No Data" />,
@@ -112,20 +299,12 @@ export default function DataGridFlexible<RowType extends GridRowModel>({
         toolbar: {
           showQuickFilter: true,
         },
-        columnsPanel: {
-          getTogglableColumns,
-        },
       }}
     />
-
-  )
-
-  // Component implementation remains largely the same
+  );
 }
 
-// ----------------------------------------------------------------------
-
-
+// CustomToolbar component
 function CustomToolbar() {
   return (
     <GridToolbarContainer>
@@ -138,3 +317,159 @@ function CustomToolbar() {
     </GridToolbarContainer>
   );
 }
+
+
+// const baseColumns: GridColDef[] = [
+//   {
+//     field: 'id',
+//     headerName: 'Id',
+//     filterable: false,
+//   },
+//   {
+//     field: 'name',
+//     headerName: 'Name',
+//     flex: 1,
+//     minWidth: 160,
+//     hideable: false,
+//     renderCell: (params) => (
+//       <Stack spacing={2} direction="row" alignItems="center" sx={{ minWidth: 0 }}>
+//         <Avatar alt={params.row.name} sx={{ width: 36, height: 36 }}>
+//           {params.row.name.charAt(0).toUpperCase()}
+//         </Avatar>
+//         <Typography component="span" variant="body2" noWrap>
+//           {params.row.name}
+//         </Typography>
+//       </Stack>
+//     ),
+//   },
+//   {
+//     field: 'email',
+//     headerName: 'Email',
+//     flex: 1,
+//     minWidth: 160,
+//     editable: true,
+//     renderCell: (params) => (
+//       <Link color="inherit" noWrap>
+//         {params.row.email}
+//       </Link>
+//     ),
+//   },
+//   {
+//     type: 'dateTime',
+//     field: 'lastLogin',
+//     headerName: 'Last login',
+//     align: 'right',
+//     headerAlign: 'right',
+//     width: 120,
+//     renderCell: (params) => (
+//       <Stack sx={{ textAlign: 'right' }}>
+//         <Box component="span">{fDate(params.row.lastLogin)}</Box>
+//         <Box component="span" sx={{ color: 'text.secondary', typography: 'caption' }}>
+//           {fTime(params.row.lastLogin)}
+//         </Box>
+//       </Stack>
+//     ),
+//   },
+//   {
+//     type: 'number',
+//     field: 'rating',
+//     headerName: 'Rating',
+//     width: 140,
+//     renderCell: (params) => (
+//       <Rating size="small" value={params.row.rating} precision={0.5} readOnly />
+//     ),
+//   },
+//   {
+//     type: 'singleSelect',
+//     field: 'status',
+//     headerName: 'Status',
+//     align: 'center',
+//     headerAlign: 'center',
+//     width: 100,
+//     editable: true,
+//     valueOptions: ['online', 'alway', 'busy'],
+//     renderCell: (params) => (
+//       <Label
+//         variant="soft"
+//         color={
+//           (params.row.status === 'busy' && 'error') ||
+//           (params.row.status === 'alway' && 'warning') ||
+//           'success'
+//         }
+//         sx={{ mx: 'auto' }}
+//       >
+//         {params.row.status}
+//       </Label>
+//     ),
+//   },
+//   {
+//     type: 'boolean',
+//     field: 'isAdmin',
+//     align: 'center',
+//     headerAlign: 'center',
+//     width: 80,
+//     renderCell: (params) =>
+//       params.row.isAdmin ? (
+//         <Iconify icon="eva:checkmark-circle-2-fill" sx={{ color: 'primary.main' }} />
+//       ) : (
+//         '-'
+//       ),
+//   },
+//   {
+//     type: 'number',
+//     field: 'performance',
+//     headerName: 'Performance',
+//     align: 'center',
+//     headerAlign: 'center',
+//     width: 160,
+//     renderCell: (params) => (
+//       <Stack spacing={1} direction="row" alignItems="center" sx={{ px: 1, width: 1, height: 1 }}>
+//         <LinearProgress
+//           value={params.row.performance}
+//           variant="determinate"
+//           color={
+//             (params.row.performance < 30 && 'error') ||
+//             (params.row.performance > 30 && params.row.performance < 70 && 'warning') ||
+//             'primary'
+//           }
+//           sx={{ width: 1, height: 6 }}
+//         />
+//         <Typography variant="caption" sx={{ width: 80 }}>
+//           {fPercent(params.row.performance)}
+//         </Typography>
+//       </Stack>
+//     ),
+//   },
+//   {
+//     type: 'actions',
+//     field: 'actions',
+//     headerName: 'Actions',
+//     align: 'right',
+//     headerAlign: 'right',
+//     width: 80,
+//     sortable: false,
+//     filterable: false,
+//     disableColumnMenu: true,
+//     getActions: (params) => [
+//       <GridActionsCellItem
+//         showInMenu
+//         icon={<Iconify icon="solar:eye-bold" />}
+//         label="View"
+//         onClick={() => console.info('VIEW', params.row.id)}
+//       />,
+//       <GridActionsCellItem
+//         showInMenu
+//         icon={<Iconify icon="solar:pen-bold" />}
+//         label="Edit"
+//         onClick={() => console.info('EDIT', params.row.id)}
+//       />,
+//       <GridActionsCellItem
+//         showInMenu
+//         icon={<Iconify icon="solar:trash-bin-trash-bold" />}
+//         label="Delete"
+//         onClick={() => console.info('DELETE', params.row.id)}
+//         sx={{ color: 'error.main' }}
+//       />,
+//     ],
+//   },
+// ];
--- src/sections/reports/answers-component/answers-list-view.tsx
+++ src/sections/reports/answers-component/answers-list-view.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import {
     Card,
@@ -12,12 +12,11 @@ import { useBoolean } from "src/hooks/use-boolean";
 import { useReports } from "src/hooks/realm/report/use-report-graphql";
 
 import uuidv4 from "src/utils/uuidv4";
-import { fDateTime } from "src/utils/format-time";
-import { formatFilterAndRemoveFields } from "src/utils/helpers";
 
 import { DataGridFlexible } from "src/components/data-grid";
 import { useSettingsContext } from "src/components/settings";
 import { LoadingScreen } from "src/components/loading-screen";
+import { IColumn } from "src/components/data-grid/data-grid-flexible";
 
 import { IReportQuestion } from "src/types/realm/realm-types";
 
@@ -35,71 +34,16 @@ export default function AnswersGridView({ id, questions }: { id?: string, questi
     const loadingReport = useBoolean()
 
     const [answers, setAnswers] = useState<AnswerObject[] | null>(null);
+
+    const [columns, setColumns] = useState<IColumn[] | null>(null);
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [reportAnswersError, setReportAnswerError] = useState(null)
 
     const showLoader = useShowLoader(loadingReport.value, 500)
 
-    const cleanedReports = useMemo(() => {
-        if (!Array.isArray(answers)) return []
-        const filtered = formatFilterAndRemoveFields(
-            answers,
-            // @ts-expect-error expected
-            ["__typename"],
-            [
-                {
-                    key: "updatedAt",
-                    formatter: fDateTime,
-                },
-                {
-                    key: "createdAt",
-                    formatter: fDateTime,
-                }
-            ],
-            undefined,
-            ["name", "creator"]
-        ) ?? []
-        return filtered
-    }, [answers])
 
 
-    // useEffect(() => {
-    //     console.log("TEST")
-    //     if (id && questions) {
-    //         loadingReport.onTrue();
-    //         setReportAnswerError(null);
-    //         getReportAnswers(id.toString()).then(res => {
-    //             const flattenedAnswers = flatten(res.map(x => x.answers));
-    //             // First, sort the questions based on their 'order' key
-    //             const sortedQuestions = questions.slice().sort((a, b) => {
-    //                 // Treat undefined order as greater than any number
-    //                 const orderA = a.order === undefined ? Number.MAX_SAFE_INTEGER : a.order;
-    //                 const orderB = b.order === undefined ? Number.MAX_SAFE_INTEGER : b.order;
-
-    //                 return orderA - orderB;
-    //             });
-
-    //             // Define the type for the accumulator and the objects it will contain
-    //             type AnswerObject = { [key: string]: string };
-
-    //             const matchedAnswers = flattenedAnswers.reduce<AnswerObject[]>((acc, answer) => {
-    //                 const matchedQuestion = sortedQuestions.find(question => question._id.toString() === answer.question_id.toString());
-    //                 if (matchedQuestion) {
-    //                     // Add the matched question-answer pair to the accumulator array
-    //                     acc.push({ [matchedQuestion.text]: answer.answer });
-    //                 }
-    //                 return acc;
-    //             }, []); // Initialize the accumulator as an empty array of AnswerObjects
-
-    //             setAnswers(matchedAnswers);
-    //         }).catch(e => {
-    //             setReportAnswerError(e.message)
-    //             console.error("FAILED TO FETCH ANSWERS", e.message)
-    //         }).finally(() => {
-    //             loadingReport.onFalse()
-    //         })
-    //     }
-    // }, [id])
     useEffect(() => {
         if (id && questions) {
             loadingReport.onTrue();
@@ -109,25 +53,42 @@ export default function AnswersGridView({ id, questions }: { id?: string, questi
                 const resAnswers = res.map(x => x.answers);
                 console.log(res, 'RES')
 
-                const qObj: { [key: string]: { text: string, order: number } } = {}
+                // const qObj: { [key: string]: { text: string, order: number } } = {}
 
-                questions.forEach(x => {
-                    qObj[x._id.toString()] = {
-                        text: x.text,
-                        order: x.order
+                // questions.forEach(x => {
+                //     qObj[x._id.toString()] = {
+
+                //         field: x._id,
+                //         label: x.text,
+                //         order: x.order,
+                //         type: x.input_type
+                //     }
+                // })
+                const d: IColumn[] = questions.map(z => {
+                    const r = {
+                        field: z._id.toString(),
+                        label: z.text,
+                        order: z.order,
+                        type: z.input_type
                     }
-                })
+                    return r;
+                }
+                )
 
-                console.log(qObj, 'QS')
+                setColumns(d);
+
+                console.log(d, 'COLUMN')
 
                 const answs = resAnswers.map((x) => {
                     const t: { _id: string, [key: string]: any } = {
                         _id: uuidv4(),
                     }; // Define `t` to explicitly allow string keys and any value
 
-                    x.map(z => {
-                        t[z.question_text] = JSON.parse(z.answer);
-                        return t;
+                    d.forEach(z => {
+                        const val = x.find(y => y.question_id.toString() === z.field.toString())
+                        if (val) {
+                            t[z.field] = JSON.parse(val.answer);
+                        }
                     })
                     return t
                 })
@@ -162,7 +123,7 @@ export default function AnswersGridView({ id, questions }: { id?: string, questi
                 }}
             >
                 {showLoader && <LoadingScreen />}
-                {!showLoader && <DataGridFlexible data={cleanedReports} getRowIdFn={(row) => row._id.toString()} />}
+                {!showLoader && columns && answers && <DataGridFlexible data={answers} getRowIdFn={(row) => row._id.toString()} columns={columns} />}
             </Card>
         </Container>
     );
